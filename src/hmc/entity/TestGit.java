e7e684e30003cb252b1a3f4b2a4783b20f7e3272
haimanchen <haimanchen627@gmail.com>
2017-03-08 09:08:15 +0800

    first commit

---
+++ src/hmc/entity/TestGit.java
@@ -0,0 +1,11 @@
+package hmc.entity;
+
+/**
+ * @author haimanchen
+ * @Date 2017/3/8 9:04
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
